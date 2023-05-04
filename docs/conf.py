19d049349bbb401aceb769ced585497d1bb26861
s godin <pub.sg@free.fr>
2023-05-04 16:40:08 +0000

    logo 4

--- docs/conf.py
+++ docs/conf.py
@@ -71,11 +71,11 @@ html_theme = 'sphinx_rtd_theme'
 #html_static_path = ['_static']
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #html_theme_path = ["_themes"]
-html_logo = 'IMG/logo.svg' 
+#html_logo = 'IMG/logo.svg' 
 
 html_theme_options = {
     'logo_only': True,
-    'display_version': False,
+    'display_version': True,
 }
 
 
