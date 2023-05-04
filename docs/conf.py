58386513e51f573b68d1a8d0fcbb70778be59d40
s godin <pub.sg@free.fr>
2023-05-04 16:03:33 +0000

    test logo

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_static_path = ['_static']
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #html_theme_path = ["_themes"]
-#html_logo = 'img/logo.svg'
+html_logo = 'img/logo.svg'
 
 html_theme_options = {
     'logo_only': True,
